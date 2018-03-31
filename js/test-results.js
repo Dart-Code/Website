26143bb3e5c68199d2f6693698a3aafbab442f08
Danny Tuppeny <danny@tuppeny.com>
2018-03-31 15:35:36 +0100

    Add a basic test results page
    For easier reviewing of test results group by stable/dev/insiders. Query String should be branch/hash.

---
+++ js/test-results.js
@@ -0,0 +1,174 @@
+var bucketRoot = "https://test-results.dartcode.org/";
+var results = [];
+var outstandingRequests = 0;
+getXml(bucketRoot, handleFileListing, console.error);
+
+function getXml(url, success, error) {
+	var request = new XMLHttpRequest();
+	request.open("GET", url);
+	request.onerror = error;
+	request.onload = function () {
+		if (this.status >= 200 && this.status < 400) {
+			success(this.responseXML);
+		} else {
+			error(this.statusText);
+		}
+	};
+	request.send();
+}
+
+function handleFileListing(xml) {
+	var files = [];
+	var matchedFiles = 0;
+	for (var file of xml.querySelectorAll("Contents")) {
+		var path = file.querySelector("Key").textContent;
+		var pathSegments = path.split("/");
+		var testSegments = pathSegments[3].substring(0, pathSegments[3].lastIndexOf(".")).split("_");
+
+		var branch = pathSegments[0];
+		var commit = pathSegments[1];
+		var os = pathSegments[2];
+		var suite = testSegments.slice(0, testSegments.length - 2).join(" ");
+		var dartVersion = testSegments[testSegments.length - 2];
+		var codeVersion = testSegments[testSegments.length - 1];
+
+		if ("?" + branch + "/" + commit !== window.location.search)
+			continue;
+
+		matchedFiles++;
+		loadResults(bucketRoot + path, suite, dartVersion, codeVersion, os);
+	}
+
+	if (matchedFiles === 0) {
+		// TODO: Handle no matched files, or drawing file list.
+	}
+}
+
+function loadResults(path, suite, dartVersion, codeVersion, os) {
+	outstandingRequests++;
+	getXml(path, function (xml) {
+		handleTestResults(suite, dartVersion, codeVersion, os, xml);
+		outstandingRequests--;
+		if (outstandingRequests == 0) {
+			updateResults();
+		}
+	}, console.error);
+}
+
+function handleTestResults(suite, dartVersion, codeVersion, os, xml) {
+	var suiteResults = results.find((r) => r.suite == suite);
+	if (!suiteResults) {
+		suiteResults = { suite: suite, testClasses: [] };
+		results.push(suiteResults);
+	}
+
+	for (var test of xml.querySelectorAll("testcase")) {
+		var className = test.getAttribute("classname");
+		var classResults = suiteResults.testClasses.find((r) => r.className == className);
+		if (!classResults) {
+			classResults = { className: className, tests: [] };
+			suiteResults.testClasses.push(classResults);
+		}
+
+		var testName = test.getAttribute("name");
+		var testResults = classResults.tests.find((r) => r.testName == testName);
+		if (!testResults) {
+			testResults = { testName: testName };
+			classResults.tests.push(testResults);
+		}
+
+		testResults[dartVersion + "_" + codeVersion + "_" + os] = {
+			time: test.getAttribute("time"),
+			failure: test.querySelector("failure") ? test.querySelector("failure").textContent : undefined
+		};
+	}
+}
+
+function updateResults() {
+	var table = document.querySelector("#test-results");
+	var tbody = table.querySelector("tbody");
+	var totalCols = 18;
+	for (var suite of results) {
+		addRow(tbody, 0, totalCols, suite.suite, "suite");
+		for (var testClass of suite.testClasses) {
+			addRow(tbody, 1, totalCols - 1, testClass.className);
+			for (var test of testClass.tests) {
+				var row = addRow(tbody, 2, 1, test.testName);
+				for (var codeVersion of ["stable", "insiders"]) {
+					for (var dartVersion of ["stable", "dev"]) {
+						// Don't show dev/dev for simplicity.
+						if (codeVersion == "insiders" && dartVersion == "dev")
+							continue;
+						row.appendChild(document.createElement("td"));
+						for (var os of ["win", "osx", "linux"]) {
+							var id = dartVersion + "_" + codeVersion + "_" + os;
+							var result = test[id];
+							var resultClassName = result ? (result.failure ? "fail" : "pass") : "unknown";
+							row.appendChild(document.createElement("td")).className = resultClassName;
+
+							// Add to column header.
+							document.getElementById(id).classList.add(resultClassName);
+						}
+						row.appendChild(document.createElement("td"));
+					}
+				}
+			}
+		}
+	}
+	document.getElementById("test-results-loading").classList.add("hide");
+	table.classList.remove("hide");
+}
+
+function addRow(table, pad, cols, label, className) {
+	var row = table.appendChild(document.createElement("tr"));
+	row.className = className;
+	for (var i = 0; i < pad; i++)
+		row.appendChild(document.createElement("td"));
+
+	var cell = row.appendChild(document.createElement("td"));
+	cell.textContent = label;
+	cell.colSpan = cols;
+
+	table.appendChild(row);
+	return row;
+}
+
+/*
+	[
+		{
+			suite: dart_only
+			tests: [
+				{
+					class: "dart_hover_provider",
+					name: "returns expected information for a void returning method",
+					results: {
+						stable_stable: {
+							win: {
+								time: 1.2,
+								result: pass,
+								error: xxx
+							},
+							linux: {
+								time: 1.2,
+								result: pass,
+								error: xxx
+							},
+						},
+						stable_unstable: {
+							win: {
+								time: 1.2,
+								result: pass,
+								error: xxx
+							},
+							linux: {
+								time: 1.2,
+								result: pass,
+								error: xxx
+							},
+						}
+					}
+				}
+			]
+		}
+	]
+*/
---
+++ test-results.md
@@ -0,0 +1,41 @@
+---
+title: Test Results
+body-class: big-screen
+---
+
+<script src="/js/test-results.js"></script>
+
+<div id="test-results-loading" class="loading"></div>
+
+<table id="test-results" class="hide">
+	<thead>
+		<tr>
+			<th colspan="3"></th>
+			<th colspan="5" class="h">Stable</th>
+			<th colspan="5" class="h">Dev Dart</th>
+			<th colspan="5" class="h">Dev Code</th>
+		</tr>
+		<tr>
+			<th class="i"></th>
+			<th class="i"></th>
+			<th></th>
+			<th class="r"></th>
+			<th class="r" id="stable_stable_win"><img class="x" src="/images/win.svg" /></th>
+			<th class="r" id="stable_stable_osx"><img class="x" src="/images/macos.svg" /></th>
+			<th class="r" id="stable_stable_linux"><img class="x" src="/images/linux.svg" /></th>
+			<th class="r"></th>
+			<th class="r"></th>
+			<th class="r" id="dev_stable_win"><img class="x" src="/images/win.svg" /></th>
+			<th class="r" id="dev_stable_osx"><img class="x" src="/images/macos.svg" /></th>
+			<th class="r" id="dev_stable_linux"><img class="x" src="/images/linux.svg" /></th>
+			<th class="r"></th>
+			<th class="r"></th>
+			<th class="r" id="stable_insiders_win"><img class="x" src="/images/win.svg" /></th>
+			<th class="r" id="stable_insiders_osx"><img class="x" src="/images/macos.svg" /></th>
+			<th class="r" id="stable_insiders_linux"><img class="x" src="/images/linux.svg" /></th>
+			<th class="r"></th>
+		</tr>
+	</thead>
+	<tbody>
+	</tbody>
+</table>
